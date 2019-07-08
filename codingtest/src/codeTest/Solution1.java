eef11ab8af69afff9dfb2bd8b3c65c229c9c6059
hsyun89 <hsyun89@naver.com>
2019-07-08 10:35:14 +0900

    .

--- codingtest/src/codeTest/Solution1.java
+++ codingtest/src/codeTest/Solution1.java
@@ -3,7 +3,7 @@ package codeTest;
 class Solution1 {
 	  public String solution(String s, int n) {
 	      char[] arr = new char[] {};
-	      
+	      System.out.println("깃허브테스트");
 	      String answer = "";
 	      return answer;
 	  }
